1def6b784a1a527e7c5f0f3685c59da5f4c8e522
eunice <m.eunice.reyes@gmail.com>
2017-03-07 21:22:18 -0800

    create a copy of index for testing

--- Desktop/vacation/js/scripts.js
+++ Desktop/vacation/js/scripts.js
@@ -1,32 +1,22 @@
 $(function() {
-  $("button").click(function(event) {
+  $(".btn").submit(function(event) {
     var nameInput = $("input#name").val();
     var weatherInput = $("input:radio[name=weather]:checked").val();
     var foodInput = $("input:radio[name=food]:checked").val();
-    var ambianceInput = $("input:radio[name=ambiance:checked").val();
+    var ambianceInput = $("input:radio[name=ambiance]:checked").val();
     var activitiesInput = $("input:radio[name=activities]:checked").val();
     var typeInput = $("input:radio[name=type]:checked").val();
 
-    // var aInput=("You should go to Thailand").val();
-    // var bInput=("You should go to France").val();
-    // var cInput=("You should go to Italy").val();
+    // var thailandInput="You should go to Thailand"
+    // var franceInput="You should go to France"
+    // var italyInput="You should go to Italy"
 
- $(".clearcl").hide();
+
 if (weatherInput==="Tropical"){
       $(".destination1").show();
-    alert("you should go to Thailand");
-}
- else if (foodInput==="Spicy Asian flavors"){
-        $(".destination1").show();
-    }
-else if (ambianceInput ==="c"){
-      $("tryResult").text(cInput).show();
-}
-else if (activitiesInput ==="a"){
-      $("tryResult").text(aInput).show();
-}
-else (typeInput ==="b"){
-      $("tryResult").text(bInput).show();
+
+else if (foodInput==="Asian"){
+    $(".destination2").show();
 }
 
   event.preventDefault();
